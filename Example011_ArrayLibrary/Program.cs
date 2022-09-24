84d904fdeece4f18e5a9d4d4fe0d044d79554db2
Nadezhda26m <nadezhda-26m@mail.ru>
2022-09-24 19:45:42 +0300

    Example 011 Array Library Fill Print

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,32 @@
+﻿// Работа с массивами, способ задания массива
+// Метод заполнения массива случайными числами
+// Метод, который распечатает массив по порядку
+
+// Метод заполнения массива случайными числами
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+// Метод, который печатает массив
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+// Задали массив из 10 элементов, заполнили и распечатали его
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
