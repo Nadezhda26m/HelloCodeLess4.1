7cb3f7cfd30bde73e539b27cb98e42ecf600b54c
Nadezhda26m <nadezhda-26m@mail.ru>
2022-09-24 21:17:45 +0300

    Add Method IndexOf Array Element Find

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Работа с массивами, способ задания массива
 // Метод заполнения массива случайными числами
 // Метод, который распечатает массив по порядку
+// Метод нахождения индекса первого подходящего элемента
 
 // Метод заполнения массива случайными числами
 void FillArray(int[] collection)
@@ -26,7 +27,31 @@ void PrintArray(int[] col)
     }
 }
 
+// Метод нахождения индекса первого подходящего элемента
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // Если find в массиве не окажется
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break; // остановит на первом вхождении
+        }
+        index++;
+    }
+    return position;
+}
+
 // Задали массив из 10 элементов, заполнили и распечатали его
 int[] array = new int[10];
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+// Выведем позицию в массиве заданного числа
+// int pos = IndexOf(array, 4);
+// Console.WriteLine(pos);
+Console.WriteLine(IndexOf(array, 4));
+// Если заданного числа в массиве нет, то покажется позиция -1
