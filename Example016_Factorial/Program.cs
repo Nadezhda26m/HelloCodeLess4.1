e1978430c9a1a835ae09a7308c92eb4526824162
Nadezhda26m <nadezhda-26m@mail.ru>
2022-10-15 23:01:21 +0300

    Example 016 Factorial

---
+++ Example016_Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вычисление факториала числа
+// с использованием рекурсии
+
+Console.Clear();
+double Factorial(int number) // в int влезают только числа до 16-17!
+{
+    if (number == 1) return 1;
+    else return number * Factorial(number - 1);
+}
+
+// Console.WriteLine(Factorial(4));
+
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine(Factorial(i));
+}
